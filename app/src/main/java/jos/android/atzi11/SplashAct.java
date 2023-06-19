42d569acc0e235d8427f1a8ead3cf8920a40b741
Josue Ruiz <40045797+Joshowmi@users.noreply.github.com>
2023-06-19 08:12:03 -0600

    Pantalla de logueo con numero celular

--- app/src/main/java/jos/android/atzi11/SplashAct.java
+++ app/src/main/java/jos/android/atzi11/SplashAct.java
@@ -2,7 +2,9 @@ package jos.android.atzi11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashAct extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SplashAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashAct.this, LoginConCelular.class));
+                finish();
+            }
+        },3000);
     }
 }
