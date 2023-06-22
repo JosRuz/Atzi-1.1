e9616726e02714174fe34039f44baefebd6490fa
Josue Ruiz <40045797+Joshowmi@users.noreply.github.com>
2023-06-21 19:18:41 -0600

    Integracion con Firebase exitosa

--- app/src/main/java/jos/android/atzi11/loginconOTP.java
+++ app/src/main/java/jos/android/atzi11/loginconOTP.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class loginconOTP extends AppCompatActivity {
 
     String numeroCelular;
@@ -16,6 +21,8 @@ public class loginconOTP extends AppCompatActivity {
 
         numeroCelular=getIntent().getExtras().getString("numero");
         Toast.makeText(this, numeroCelular, Toast.LENGTH_LONG).show();
+
+
 
     }
 }
