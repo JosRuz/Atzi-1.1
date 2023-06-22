5f5c2600d0de47031c8f633a88c10b939e04546b
Josue Ruiz <40045797+Joshowmi@users.noreply.github.com>
2023-06-21 19:07:15 -0600

    Validacion local de numero celular

--- app/src/main/java/jos/android/atzi11/LoginConCelular.java
+++ app/src/main/java/jos/android/atzi11/LoginConCelular.java
@@ -2,13 +2,47 @@ package jos.android.atzi11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
 
 public class LoginConCelular extends AppCompatActivity {
+
+    CountryCodePicker codigoDePais;
+    EditText numeroCelular;
+    Button enviarCodigoBtn;
+    ProgressBar barraProgreso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_con_celular);
+
+        codigoDePais=findViewById(R.id.txt_codigopais);
+        numeroCelular=findViewById(R.id.txt_numero_telefono);
+        enviarCodigoBtn=findViewById(R.id.btn_enviar_codigo);
+        barraProgreso=findViewById(R.id.pb_progreso_enviar_codigo);
+
+        barraProgreso.setVisibility(View.GONE);
+
+        codigoDePais.registerCarrierNumberEditText(numeroCelular);
+        enviarCodigoBtn.setOnClickListener((v)->{
+            if (!codigoDePais.isValidFullNumber()){
+
+                numeroCelular.setError("Numero de celular no valido");
+                return;
+
+            }
+            Intent intent=new Intent(LoginConCelular.this, loginconOTP.class);
+            intent.putExtra("numero",codigoDePais.getFullNumberWithPlus());
+
+            startActivity(intent);
+        });
+
     }
 }
--- app/src/main/java/jos/android/atzi11/loginconOTP.java
+++ app/src/main/java/jos/android/atzi11/loginconOTP.java
@@ -3,12 +3,19 @@ package jos.android.atzi11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class loginconOTP extends AppCompatActivity {
+
+    String numeroCelular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logincon_otp);
+
+        numeroCelular=getIntent().getExtras().getString("numero");
+        Toast.makeText(this, numeroCelular, Toast.LENGTH_LONG).show();
+
     }
 }
