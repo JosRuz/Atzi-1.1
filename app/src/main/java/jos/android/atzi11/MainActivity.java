dd405ceee23cac060a63f880edd6fca48b77baa6
Josue Ruiz <josu24.42@gmail.com>
2023-06-28 10:23:03 -0600

    Menu de navegacion y fragmentos para chat y perfil

--- app/src/main/java/jos/android/atzi11/MainActivity.java
+++ app/src/main/java/jos/android/atzi11/MainActivity.java
@@ -1,14 +1,54 @@
 package jos.android.atzi11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ImageButton btn_buscar;
+
+    ChatFragment chatFragment;
+    ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatFragment=new ChatFragment();
+        profileFragment=new ProfileFragment();
+
+        bottomNavigationView=findViewById(R.id.button_navigation);
+        btn_buscar=findViewById(R.id.btn_main_buscar);
+
+        btn_buscar.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this,SearchUserActivity.class));
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.menu_chat){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,chatFragment).commit();
+                }
+                if (item.getItemId()==R.id.menu_profile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,profileFragment).commit();
+                }
+                return false;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
+
     }
 }
